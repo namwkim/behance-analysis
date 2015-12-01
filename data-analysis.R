9715df316f983fa7cc06e7305f640192690a86c2
NAM WOOK KIM <namwkim@seas.harvard.edu>
2015-11-30 22:38:28 -0500

    update

---
+++ data-analysis.R
@@ -0,0 +1,15 @@
+##------------------------------
+## Author: Nam Wook Kim
+## HUID: 90948148
+## Date: Dec 10, 2015
+##------------------------------
+
+# require packages
+
+
+#
+#------ load data
+#
+data = read.csv("/Users/namwkim85/Projects/behence-analysis/behance-users.csv")
+
+
