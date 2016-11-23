e53853b44dfebf66243a1d90e62d441e70636fd6
NAM WOOK KIM <namwkim@seas.harvard.edu>
2016-11-23 13:07:25 -0500

    user analysis update

--- crawl-project-images.py
+++ crawl-project-images.py
@@ -1,17 +1,25 @@
-import sys, os, urllib, csv, urlparse, time, pymongo
-
+import sys, os, urllib, csv, urlparse, time, pymongo, random
+import numpy as np
 # db connection
 localClient = pymongo.MongoClient('localhost', 27017)
 db          = localClient.behance
 dbprojects  = db.projects
-projects = list(dbprojects.find())
+# projects = list(dbprojects.find())
+users = list(db.users.find({'gender':{'$ne':'unknown'}}))
+print 'Total users with gender:', len(users)
+projects = []
+i = 0
+for user in users:
+    i+=1
+    print i
+    projects.extend(list(db.projects.find({'owners.id':user['id']})))
 
 for idx, project in enumerate(projects):
     projID          = project['id']
-    if isinstance(project["covers"], list) or project["covers"]["original"]==None:
+    if isinstance(project["covers"], list) or project["covers"]["202"]==None:
         print 'url is not found:', project["covers"]
         continue
-    projImageURL    = project["covers"]["original"]
+    projImageURL    = project["covers"]["202"]
     ext = os.path.splitext(urlparse.urlparse(projImageURL).path)[1]
     print idx, "retrieving",str(projID)+ext
     if os.path.exists('./images/'+str(projID)+".jpg")\
