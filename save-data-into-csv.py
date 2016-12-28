d6e3e3fc0b975eb5234cb94f8b8899f2d288192d
NAM WOOK KIM <namwkim@seas.harvard.edu>
2016-12-27 19:03:36 -0500

    topic analysis started

--- save-data-into-csv.py
+++ save-data-into-csv.py
@@ -13,7 +13,7 @@ dbappreciations = db.appreciations
 print 'retrieving users...'
 users = list(db.users.find({'gender':{'$ne':'unknown'}}))
 # only 10% percent (comment this for full users)
-percent = 100
+percent = 30
 users = random.sample(users, int(len(users)*percent/100.0))
 print 'number of users:', len(users)
 userMap  = {user['id']:user for user in users}
@@ -61,7 +61,7 @@ print '#of appreciate-links:', len(filter(lambda x: x[2]=='appreciate', links))
 print '#of create-links:', len(filter(lambda x: x[2]=='create', links))
 
 # print 'save into csv files'
-userwriter = csv.writer(open("./data/sampled-graph/users-"+str(percent)+".csv", 'wb'));
+userwriter = csv.writer(open("./data/users-"+str(percent)+".csv", 'wb'));
 userwriter.writerow(['user_id', 'fields', 'followers', 'following', 'username',\
     'gender', 'country', 'comments', 'project_counts', 'project_views', \
     'project_appreciations', 'project_comments', 'created_on'])
@@ -74,14 +74,14 @@ for user in users:
     record 	=[ s.encode('utf-8') if isinstance(s, unicode) else s for s in record]
     userwriter.writerow(record)
 
-desigwriter = csv.writer(open("./data/sampled-graph/designs-"+str(percent)+".csv", 'wb'));
+desigwriter = csv.writer(open("./data/projects-"+str(percent)+".csv", 'wb'));
 desigwriter.writerow(['design_id', 'fields', 'appreciations', 'url'])
 for design in designs:
     desigwriter.writerow([design['id'], "|".join(design['fields']), \
         design['stats']['appreciations'],\
         design['url']])
 
-linkwriter = csv.writer(open("./data/sampled-graph/links-"+str(percent)+".csv", 'wb'));
+linkwriter = csv.writer(open("./data/links-"+str(percent)+".csv", 'wb'));
 linkwriter.writerow(['user_id', 'design_id', 'type'])
 for link in links:
     linkwriter.writerow([link[0], link[1], link[2]])
